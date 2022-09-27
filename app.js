0b0419269174573e07fa620790c5f1237164c553
Kevin Hanard <kevin.hanard@gmail.com>
2022-09-27 11:20:28 +1000

    first commit

---
+++ app.js
@@ -0,0 +1,158 @@
+const menu = [
+  {
+    id: 1,
+    title: 'buttermilk pancakes',
+    category: 'breakfast',
+    price: 15.99,
+    img: './images/item-1.jpeg',
+    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+  },
+  {
+    id: 2,
+    title: 'diner double',
+    category: 'lunch',
+    price: 13.99,
+    img: './images/item-2.jpeg',
+    desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+  },
+  {
+    id: 3,
+    title: 'godzilla milkshake',
+    category: 'shakes',
+    price: 6.99,
+    img: './images/item-3.jpeg',
+    desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+  },
+  {
+    id: 4,
+    title: 'country delight',
+    category: 'breakfast',
+    price: 20.99,
+    img: './images/item-4.jpeg',
+    desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+  },
+  {
+    id: 5,
+    title: 'egg attack',
+    category: 'lunch',
+    price: 22.99,
+    img: './images/item-5.jpeg',
+    desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+  },
+  {
+    id: 6,
+    title: 'oreo dream',
+    category: 'shakes',
+    price: 18.99,
+    img: './images/item-6.jpeg',
+    desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+  },
+  {
+    id: 7,
+    title: 'bacon overflow',
+    category: 'breakfast',
+    price: 8.99,
+    img: './images/item-7.jpeg',
+    desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+  },
+  {
+    id: 8,
+    title: 'american classic',
+    category: 'lunch',
+    price: 12.99,
+    img: './images/item-8.jpeg',
+    desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+  },
+  {
+    id: 9,
+    title: 'quarantine buddy',
+    category: 'shakes',
+    price: 16.99,
+    img: './images/item-9.jpeg',
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
+  {
+    id: 10,
+    title: 'steak dinner',
+    category: 'dinner',
+    price: 39.99,
+    img: './images/item-10.jpeg',
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
+];
+
+//select parent
+const sectionCenter = document.querySelector('.section-center');
+
+// we want to show element when page finished to load
+window.addEventListener('DOMContentLoaded', function () {
+  // //console.log('yum');
+  // let displayMenu = menu.map(function (meal) {
+  //   // we just copy/paste our html
+  //   return `<article class="menu-item">
+  //         <img src=${meal.img} class="photo" alt=${meal.title} />
+  //         <div class="item-info">
+  //           <header>
+  //             <h4>${meal.title}</h4>
+  //             <h4 class="price">${meal.price}</h4>
+  //           </header>
+  //           <p class="item-text">
+  //            ${meal.desc}
+  //           </p>
+  //         </div>
+  //       </article>`;
+  // });
+  // displayMenu = displayMenu.join(''); //concatenete displayMenu
+  // sectionCenter.innerHTML = displayMenu; //inside the section we create each element of menu using (meal) inner`HTML
+  // //console.log(displayMenu);
+
+  //after refractoring and fct creation
+  displayMenuItems(menu);
+});
+
+//refactoring
+
+function displayMenuItems(menuItems) {
+  //console.log('yum');
+  let displayMenu = menuItems.map(function (meal) {
+    // we just copy/paste our html
+    return `<article class="menu-item">
+          <img src=${meal.img} class="photo" alt=${meal.title} />
+          <div class="item-info">
+            <header>
+              <h4>${meal.title}</h4>
+              <h4 class="price">${meal.price}</h4>
+            </header>
+            <p class="item-text">
+             ${meal.desc}
+            </p>
+          </div>
+        </article>`;
+  });
+  displayMenu = displayMenu.join(''); //concatenete displayMenu
+  sectionCenter.innerHTML = displayMenu; //inside the section we create each element of menu using (meal) inner`HTML
+  //console.log(displayMenu);
+}
+
+// filter type of meal (breakfast/lunch/...)
+const typeMeal = document.querySelectorAll('.filter-btn');
+
+typeMeal.forEach(function (btn) {
+  btn.addEventListener('click', function (e) {
+    //console.log(e.currentTarget.textContent); //search for text content
+    //const category = e.currentTarget.textContent; tried with textContent
+    const category = e.currentTarget.dataset.id;
+    const menuCategory = menu.filter(function (menuItem) {
+      if (menuItem.category === category) {
+        return menuItem;
+      }
+    });
+    if (category === 'all') {
+      displayMenuItems(menu);
+      //console.log('isitworking');
+    } else {
+      displayMenuItems(menuCategory);
+      // console.log('please');
+    }
+  });
+});
